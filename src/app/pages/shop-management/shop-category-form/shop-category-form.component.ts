9381f66673b3f93d4473bd9d46cf1a32e14f4f04
FanantenanaKantoMaminiaina <fanantenanakanto27@gmail.com>
2026-01-28 16:29:41 +0300

    [ADD] : Structuration project

---
+++ src/app/pages/shop-management/shop-category-form/shop-category-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-category-form',
+  imports: [],
+  templateUrl: './shop-category-form.component.html',
+  styleUrl: './shop-category-form.component.scss'
+})
+export class ShopCategoryFormComponent {
+
+}
