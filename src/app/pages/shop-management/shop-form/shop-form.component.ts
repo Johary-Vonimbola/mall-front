4a72c2370ada2b655f5c37a9cb2338130bd6625f
FanantenanaKantoMaminiaina <fanantenanakanto27@gmail.com>
2026-01-30 09:22:48 +0300

    [ADD] : Form for shop and category

--- src/app/pages/shop-management/shop-form/shop-form.component.ts
+++ src/app/pages/shop-management/shop-form/shop-form.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { NgClass, NgIf } from '@angular/common';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ShopManagementService } from '../../../services/shop-management.service';
+import { isInvalid } from '../../../utils/form';
+import { ShopCategoryResponse } from '../../../models/shop';
 
 @Component({
   selector: 'app-shop-form',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgClass,
+    NgIf
+  ],
   templateUrl: './shop-form.component.html',
   styleUrl: './shop-form.component.scss'
 })
-export class ShopFormComponent {
+export class ShopFormComponent implements OnInit {
+  private shopManagementService: ShopManagementService = inject(ShopManagementService);
+  isInvalid = isInvalid;
 
+  categories: ShopCategoryResponse[] = [];
+
+  ngOnInit(): void {
+    this.shopManagementService.getAllShopCategory().subscribe({
+      next: (shopCategories) => {
+        this.categories = shopCategories;
+      },
+      error: () => {
+        console.error('Erreur lors du chargement des catégories');
+      }
+    });
+  }
+
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    category: new FormControl('', [Validators.required]),
+    categoryId: new FormControl('', [Validators.required])
+  });
+  errors = signal<String[]>([]);
+
+  onSubmit(): void{
+    this.form.markAllAsTouched();
+    if(this.form.invalid) return;
+    const formValue = this.form.value;
+    this.shopManagementService.createShop(formValue).subscribe({
+      next: res => {
+        
+      },
+      error: res => {
+        this.errors.set(res.error.errors)
+      }
+    });
+  }
 }
