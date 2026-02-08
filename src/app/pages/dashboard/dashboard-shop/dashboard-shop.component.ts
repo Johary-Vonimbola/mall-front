e4e534804d503aced3685efcd8770f34f57496f1
FanantenanaKantoMaminiaina <fanantenanakanto27@gmail.com>
2026-02-08 16:33:20 +0300

    [ADD] : Structure of rent

---
+++ src/app/pages/dashboard/dashboard-shop/dashboard-shop.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-shop',
+  imports: [],
+  templateUrl: './dashboard-shop.component.html',
+  styleUrl: './dashboard-shop.component.scss'
+})
+export class DashboardShopComponent {
+
+}
