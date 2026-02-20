4074de73a7ec92f625b73b8cf5a0e09b0edb6c73
Johary-Vonimbola <joharyvonimbola@gmail.com>
2026-02-20 09:17:26 +0300

    [ADD]: shop rent payements admin with filters

--- src/app/models/shop.ts
+++ src/app/models/shop.ts
@@ -5,6 +5,7 @@ export interface ShopResponse {
     category: string;
     categoryId: string;
     logo?: string;
+    _id?: string;
 }
 
 export interface ShopCategoryResponse{
---
+++ src/app/pages/shop-rent-payment/rent-payment-admin/rent-payment-admin.component.ts
@@ -0,0 +1,94 @@
+import { Component, computed, inject, signal } from '@angular/core';
+import { ShopRentPaymentService } from '../../../services/shop-rent-payment.service';
+import { ShopRentPayment } from '../../../models/shopRent';
+import { DatePipe } from '@angular/common';
+import { ShopManagementService } from '../../../services/shop-management.service';
+import { ShopResponse } from '../../../models/shop';
+import { MONTHS, YEARS } from '../../../models/month';
+
+@Component({
+  selector: 'app-rent-payment-admin',
+  imports: [
+    DatePipe
+  ],
+  templateUrl: './rent-payment-admin.component.html',
+  styleUrl: './rent-payment-admin.component.scss'
+})
+export class RentPaymentAdminComponent {
+  private rentPaymentService: ShopRentPaymentService = inject(ShopRentPaymentService);
+  private shopService = inject(ShopManagementService);
+
+  shops = signal<ShopResponse[]>([]);
+
+  rents = signal<ShopRentPayment[]>([]);
+  selectedShops = signal<string[]>([]);
+  selectedMonth = signal<number>(0);
+  selectedYear = signal<number>(0);
+  selectedStatus = signal<string>('');
+
+  months = MONTHS;
+  years = YEARS;
+
+  filteredRents = computed(() => {
+    return this.rents().filter(rent => {
+
+      const matchShop =
+        this.selectedShops().length === 0 ||
+        this.selectedShops().includes(rent.shopId?._id ?? '');
+
+      const matchMonth =
+        !this.selectedMonth() ||
+        rent.month == this.selectedMonth();
+
+      const matchYear =
+        !this.selectedYear() ||
+        rent.year == this.selectedYear();
+
+      const matchStatus =
+        !this.selectedStatus() ||
+        rent.status === this.selectedStatus();
+
+      return matchShop && matchMonth && matchYear && matchStatus;
+    });
+  });
+  loadRents(): void {
+    this.rentPaymentService.getAllRentPayments().subscribe(res => {
+      this.rents.set(res);
+    });
+  }
+  loadShops(): void {
+    this.shopService.getAllShop().subscribe(res => {
+      this.shops.set(res);
+    });
+  }
+
+  onShopToggle(event: any): void {
+    const shopId = event.target.value;
+    const checked = event.target.checked;
+
+    if (checked) {
+      this.selectedShops.set([...this.selectedShops(), shopId]);
+    } else {
+      this.selectedShops.set(
+        this.selectedShops().filter(id => id !== shopId)
+      );
+    }
+  }
+
+  onMonthChange(event: any): void {
+    this.selectedMonth.set(event.target.value);
+  }
+
+  onYearChange(event: any): void {
+    this.selectedYear.set(event.target.value);
+  }
+
+  onStatusChange(event: any): void {
+    this.selectedStatus.set(event.target.value);
+  }
+
+  ngOnInit(): void {
+    this.loadRents();
+    this.loadShops();
+  }
+}
