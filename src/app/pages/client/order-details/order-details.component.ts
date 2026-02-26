bad2b013a28877bc8e08bb639168ed2d399ffd0a
Johary-Vonimbola <joharyvonimbola@gmail.com>
2026-02-26 12:59:03 +0300

    [FIX]: pdf

--- src/app/pages/client/order-details/order-details.component.ts
+++ src/app/pages/client/order-details/order-details.component.ts
@@ -45,21 +45,40 @@ export class OrderDetailsComponent implements OnInit{
   errors = signal<String[]>([]);
   showDeliveryModal = signal(false);
   @ViewChild('content') content!: ElementRef;
-
   public generatePDF(): void {
     const elementToExport = this.content.nativeElement;
 
-    html2canvas(elementToExport).then(canvas => {
-      const imgData = canvas.toDataURL('image/png');
-      const pdf = new jsPDF('p', 'mm', 'a4');
-      const width = pdf.internal.pageSize.getWidth();
-      const height = pdf.internal.pageSize.getHeight();
+    html2canvas(elementToExport, {
+      scale: 2,
+      useCORS: true
+    }).then(canvas => {
 
-      pdf.addImage(imgData, 'PNG', 0, 0, width, height);
-      pdf.save(`${this.order()?._id}-${this.order()?.date}-${this.order()?.clientId}.pdf`);
+      const imgData = canvas.toDataURL('image/jpeg', 0.8);
+
+      const pdf = new jsPDF('p', 'mm', 'a4');
+
+      const pageWidth = pdf.internal.pageSize.getWidth();
+      const pageHeight = pdf.internal.pageSize.getHeight();
+
+      const imgWidth = pageWidth;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+      let heightLeft = imgHeight;
+      let position = 0;
+
+      pdf.addImage(imgData, 'JPEG', 0, position, imgWidth, imgHeight);
+      heightLeft -= pageHeight;
+
+      while (heightLeft > 0) {
+        position = heightLeft - imgHeight;
+        pdf.addPage();
+        pdf.addImage(imgData, 'JPEG', 0, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+      }
+
+      pdf.save(`facture-${this.order()?._id}.pdf`);
     });
   }
-
   loadOrder(id: string): void{
     this.orderService.getById(id).subscribe(res => {
       if(res){
